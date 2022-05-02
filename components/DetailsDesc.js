320862e74f30b3e9ac888e2d00accc0e22e6e253
Wiljami Takkinen <74732018+WiljamiT@users.noreply.github.com>
2022-05-02 18:17:51 +0300

    Update DetailsDesc.js

--- components/DetailsDesc.js
+++ components/DetailsDesc.js
@@ -1,62 +1,70 @@
-import { useState } from 'react';
-import { View, Text } from 'react-native';
+import { useState } from "react";
+import { View, Text } from "react-native";
 
-import { EurPrice, AlbumTitle } from './SubInfo';
-import { COLORS, SIZES, FONTS } from '../constants';
+import { EurPrice, AlbumTitle } from "./SubInfo";
+import { COLORS, SIZES, FONTS } from "../constants";
 
 const DetailsDesc = ({ data }) => {
   const [text, setText] = useState(data.description.slice(0, 100));
-  const [readMore, setReadMore] = useState(false)
+  const [readMore, setReadMore] = useState(false);
   return (
     <>
-       <View style={{
-        width: "100%", 
-        flexDirection: "row", 
-        alignItems: "center",
-        justifyContent: "center",
-        paddingBottom: 20,
-      }}>
+      <View
+        style={{
+          width: "100%",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          paddingBottom: 20,
+        }}
+      >
         <EurPrice price={data.price} />
       </View>
-      <View style={{ 
-        width: "100%", 
-        flexDirection: "row", 
-        justifyContent: "space-between",
-        alignItems: "center"
-      }}>
-        <AlbumTitle 
+      <View
+        style={{
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <AlbumTitle
           title={data.name}
           subTitle={data.seller}
           titleSize={SIZES.large}
           subTitleSize={SIZES.font}
         />
-
-        
       </View>
 
-      <View style={{ marginVertical: SIZES.extraLarge * 1.5}}>
-        <Text style={{
-          fontSize: SIZES.font,
-          fontFamily: FONTS.semiBold, 
-          color: COLORS.primary,
-        }}>Kuvaus</Text>
+      <View style={{ marginVertical: SIZES.extraLarge * 1.5 }}>
+        <Text
+          style={{
+            fontSize: SIZES.font,
+            fontFamily: FONTS.semiBold,
+            color: COLORS.primary,
+          }}
+        >
+          Kuvaus
+        </Text>
         <View style={{ marginTop: SIZES.base }}>
-          <Text style={{
-            fontSize: SIZES.small,
-            fontFamily: FONTS.regular, 
-            color: COLORS.secondary,
-            lineHeight: SIZES.large
-          }}>
+          <Text
+            style={{
+              fontSize: SIZES.small,
+              fontFamily: FONTS.regular,
+              color: COLORS.secondary,
+              lineHeight: SIZES.large,
+            }}
+          >
             {text}
-            {!readMore && '...'}
-            <Text 
+            {!readMore && "..."}
+            <Text
               style={{
                 fontSize: SIZES.small,
-                fontFamily: FONTS.semiBold, 
+                fontFamily: FONTS.semiBold,
                 color: COLORS.secondary,
               }}
               onPress={() => {
-                if(!readMore) {
+                if (!readMore) {
                   setText(data.description);
                   setReadMore(true);
                 } else {
@@ -64,14 +72,14 @@ const DetailsDesc = ({ data }) => {
                   setReadMore(false);
                 }
               }}
-              >
-              {readMore ? ' Näytä Vähemmän' : ' Lue lisää'}
+            >
+              {readMore ? " Näytä Vähemmän" : " Lue lisää"}
             </Text>
           </Text>
         </View>
       </View>
     </>
-  )
-}
+  );
+};
 
-export default DetailsDesc
+export default DetailsDesc;
